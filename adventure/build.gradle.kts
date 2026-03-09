686e17fceb710b2a4627eea76919fcd75c91ce63
peco2282 <peco2282@peco2282.com>
2026-03-09 20:29:57 +0900

    Add legacy text serializer to Adventure dependencies

--- adventure/build.gradle.kts
+++ adventure/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
   testImplementation(kotlin("test"))
   compileOnly(libs.adventure)
+  compileOnly(libs.serializer.legacy)
   testImplementation(libs.adventure)
   testImplementation(libs.serializer.plugin)
 }
