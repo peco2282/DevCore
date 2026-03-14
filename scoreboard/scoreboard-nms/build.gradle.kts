5c5a030704fb403b1ec647e056ded37477ef37c3
peco2282 <peco2282@peco2282.com>
2026-03-14 16:06:58 +0900

    Update `build.gradle.kts` to include `v1_21_4` dependency and fix `paper-api` version reference

--- scoreboard/scoreboard-nms/build.gradle.kts
+++ scoreboard/scoreboard-nms/build.gradle.kts
@@ -6,6 +6,7 @@ version = properties["devcore.scoreboard.nms.version"] ?: "1.0.0"
 
 dependencies {
   implementation(project(":scoreboard:scoreboard-api"))
+  implementation(project(":scoreboard:scoreboard-nms:v1_21_4"))
   implementation(project(":adventure"))
   implementation(project(":scheduler"))
   compileOnly(libs.paper.api)
@@ -17,5 +18,5 @@ val paperVersion = "1.21.4-R0.1-SNAPSHOT"
 
 dependencies {
   paperweight.paperDevBundle(paperVersion)
-  compileOnly("io.papermc.paper:paper-api:${version}")
+  compileOnly("io.papermc.paper:paper-api:${paperVersion}")
 }
