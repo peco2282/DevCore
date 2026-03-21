25527988544d9a68e1e152613b1da6465aa0a051
peco2282 <peco2282@peco2282.com>
2026-03-21 16:38:37 +0900

    Clean up imports and fix minor formatting issue in `Effects.kt`.

--- effect/src/main/kotlin/com/peco2282/devcore/effect/Effects.kt
+++ effect/src/main/kotlin/com/peco2282/devcore/effect/Effects.kt
@@ -2,16 +2,15 @@ package com.peco2282.devcore.effect
 
 import org.bukkit.Location
 import org.bukkit.Particle
-import org.bukkit.World
 
 /**
  * Utility for particle effects.
  */
 object Effects {
-    /**
-     * Spawn a cloud of particles at the given location.
-     */
-    fun spawnCloud(location: Location, particle: Particle = Particle.CLOUD, count: Int = 10) {
-        location.world?.spawnParticle(particle, location, count, 0.2, 0.2, 0.2, 0.05)
-    }
+  /**
+   * Spawn a cloud of particles at the given location.
+   */
+  fun spawnCloud(location: Location, particle: Particle = Particle.CLOUD, count: Int = 10) {
+    location.world?.spawnParticle(particle, location, count, 0.2, 0.2, 0.2, 0.05)
+  }
 }
