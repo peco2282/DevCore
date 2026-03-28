59f3efb0588b56b6d3e6effc3e0287e1fc84660e
peco2282 <peco2282@peco2282.com>
2026-03-28 18:29:32 +0900

    Rename `serializer-plugin` to `serializer-plain` in `libs.versions.toml` and update references in `adventure` module.

--- adventure/build.gradle.kts
+++ adventure/build.gradle.kts
@@ -3,8 +3,8 @@ version = properties["devcore.adventure.version"] ?: "1.0.0"
 dependencies {
   testImplementation(libs.kotlin.test)
   compileOnly(libs.adventure)
+  compileOnly(libs.serializer.plain)
   compileOnly(libs.serializer.legacy)
   compileOnly(libs.serializer.minimessage)
   testImplementation(libs.adventure)
-  testImplementation(libs.serializer.plugin)
 }
