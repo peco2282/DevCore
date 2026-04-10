b3f0b033ca39263ff0e0c6893af65a558b4e7366
peco2282 <peco2282@peco2282.com>
2026-04-10 10:24:32 +0900

    Add version checks and `UnsupportedVersionException` to helper utilities

--- util/src/main/kotlin/com/peco2282/devcore/util/helper.kt
+++ util/src/main/kotlin/com/peco2282/devcore/util/helper.kt
@@ -1,5 +1,32 @@
 package com.peco2282.devcore.util
 
+import org.bukkit.Bukkit
+
 inline fun <T, reified E: T> T.asNullable(orThrow: Boolean = false): E? = (this as? E).also { if (orThrow && it == null) throw IllegalArgumentException("Cannot cast to non-null type ${E::class.qualifiedName}") }
 
 inline fun <T, reified E: T> T.asNotNullable(): E = this as E
+
+class UnsupportedVersionException(current: String, target: String) : RuntimeException("This feature is not supported in the $current version of Minecraft. Please update to the $target version or higher of Minecraft.")
+
+@Throws(UnsupportedVersionException::class)
+fun checkVersion(target: String) {
+  val current = Bukkit.getServer().minecraftVersion
+
+  if (!isVersionAtLeast(current, target)) {
+    throw UnsupportedVersionException(current, target)
+  }
+}
+
+private fun isVersionAtLeast(current: String, target: String): Boolean {
+  val currentParts = current.split('.')
+  val targetParts = target.split('.')
+
+  for (i in targetParts.indices) {
+    val currentPart = currentParts.getOrNull(i)?.toIntOrNull() ?: 0
+    val targetPart = targetParts[i].toInt()
+
+    if (currentPart < targetPart) return false
+  }
+
+  return true
+}
