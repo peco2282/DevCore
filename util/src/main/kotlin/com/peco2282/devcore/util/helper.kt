5e4f0bb081cb6f4b4935648aa3999b3fb57ebc76
peco2282 <peco2282@peco2282.com>
2026-04-12 12:17:04 +0900

    Add coroutine utilities for Bukkit plugins, including dispatcher, scope, and `launch` extension

--- util/src/main/kotlin/com/peco2282/devcore/util/helper.kt
+++ util/src/main/kotlin/com/peco2282/devcore/util/helper.kt
@@ -1,6 +1,15 @@
 package com.peco2282.devcore.util
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 inline fun <T, reified E: T> T.asNullable(orThrow: Boolean = false): E? = (this as? E).also { if (orThrow && it == null) throw IllegalArgumentException("Cannot cast to non-null type ${E::class.qualifiedName}") }
 
@@ -33,3 +42,32 @@ private fun isVersionAtLeast(current: String, target: String): Boolean {
 
 @RequiresOptIn("This API is internal to DevCore and may change without a major version bump.")
 annotation class DevCoreInternal
+
+internal val coroutinesCatch = mutableMapOf<Plugin, CoroutineDispatcher>()
+
+val Plugin.coroutine: CoroutineDispatcher get() = coroutinesCatch.getOrPut(this) { BukkitDispatcher(this) }
+
+internal class BukkitDispatcher(val plugin: Plugin): CoroutineDispatcher() {
+  override fun dispatch(context: CoroutineContext, block: Runnable) {
+    if (Bukkit.isPrimaryThread()) {
+      block.run()
+    } else {
+      Bukkit.getScheduler().runTask(plugin, block)
+    }
+  }
+}
+
+internal val scopeMap = mutableMapOf<Plugin, CoroutineScope>()
+
+val Plugin.scope: CoroutineScope
+  get() = scopeMap.getOrPut(this) {
+    CoroutineScope(SupervisorJob() + coroutine)
+  }
+
+
+fun Plugin.launch(
+  context: CoroutineContext = EmptyCoroutineContext,
+  block: suspend CoroutineScope.() -> Unit
+): Job {
+  return this.scope.launch(context, block = block)
+}
